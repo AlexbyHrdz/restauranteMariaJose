89ec4697c8be28517c953564244b862d15cd305e
AlexbyHrdz <alexby11ehn@gmail.com>
2023-10-20 10:27:29 -0500

    m1
    act

---
+++ ProyectoMAjo/src/main/java/co/com/proyecto/registros/Administrador.java
@@ -0,0 +1,5 @@
+package co.com.proyecto.registros;
+
+public class Administrador {
+
+}
---
+++ ProyectoMAjo/src/main/java/co/com/proyecto/registros/Registro.java
@@ -0,0 +1,8 @@
+package co.com.proyecto.registros;
+
+public class Registro {
+   protected int id;
+   protected  String nombre;
+   protected String apellido;
+
+}
