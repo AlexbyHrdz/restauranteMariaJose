996dd7a595e2b96576f108795928c8eae8e8f10c
joha2424 <139502619+DecyTru@users.noreply.github.com>
2023-10-20 10:52:34 -0500

    m5
    act1
    
    Co-Authored-By: AlexbyHrdz <142028689+AlexbyHrdz@users.noreply.github.com>
    Co-Authored-By: pablolotero <141444070+pablolotero@users.noreply.github.com>

--- ProyectoMAjo/src/main/java/co/com/proyecto/registros/Administrador.java
+++ ProyectoMAjo/src/main/java/co/com/proyecto/registros/Administrador.java
@@ -2,4 +2,7 @@ package co.com.proyecto.registros;
 
 public class Administrador {
 
+    int cargo;
+
+
 }
--- ProyectoMAjo/src/main/java/co/com/proyecto/registros/Usuarios.java
+++ ProyectoMAjo/src/main/java/co/com/proyecto/registros/Usuarios.java
@@ -2,6 +2,16 @@ package co.com.proyecto.registros;
 
 public class Usuarios extends Registro{
 
-     
+    public static void ordenarPedido(){
 
+    }
+    public static void seleccionarMesa(){
+
+    }
+    public static void Comentario(){
+
+    }
+    public static void visualizarFactura(){
+
+    }
 }
